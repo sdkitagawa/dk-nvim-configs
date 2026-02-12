11157418211e17c957ed90c6a0f40dd6e205282f
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-02-13 06:11:58 +0900

    chore: remove cord.nvim setup from init.lua (nvchad neovim config)
    - Removed Cord.nvim configuration from the main initialization file.
    - Cleaned up related settings and dependencies from `init.lua`.

--- Windows/nvim-nvchad/lua/plugins/init.lua
+++ Windows/nvim-nvchad/lua/plugins/init.lua
@@ -11,31 +11,7 @@ return {
     config = function()
       require "configs.lspconfig"
     end,
-  },
-
-  -- Cord Setup
-{
-  "vyfor/cord.nvim",
-  lazy = false,
-  build = ":Cord update",
-  config = function()
-    require("cord").setup {
-      enabled = true,
-      log_level = vim.log.levels.OFF,
-      editor = { client = "neovim", tooltip = "Neovim" },
-      display = { theme = "default", flavor = "dark", view = "full" },
-      timestamp = { enabled = true },
-      idle = { enabled = true, timeout = 300000, details = "Idling", tooltip = "💤" },
-      text = {
-        workspace = function(opts) return "In " .. opts.workspace end,
-        viewing   = function(opts) return "Viewing " .. opts.filename end,
-        editing   = function(opts) return "Editing " .. opts.filename end,
-        dashboard = "Home",
-      },
-      buttons = nil,
-    }
-  end,
-},
+  }
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
