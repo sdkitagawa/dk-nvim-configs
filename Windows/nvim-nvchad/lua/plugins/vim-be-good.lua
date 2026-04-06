d71904a5053e5221470fd560ab62f17e9d9a8293
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-04-07 01:37:29 +0900

    Create vim-be-good.lua

---
+++ Windows/nvim-nvchad/lua/plugins/vim-be-good.lua
@@ -0,0 +1,8 @@
+return {
+    "ThePrimeagen/vim-be-good",
+    cmd = "VimBeGood",
+    config = function()
+        -- vim-be-good doesn't require setup, but NvChad prefers explicit config
+        -- The plugin auto-initializes when :VimBeGood is called
+    end,
+}
