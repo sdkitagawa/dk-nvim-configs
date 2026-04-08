da918e851a2e721aa8c84c2e622f2a9306413f5c
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-04-09 00:25:41 +0900

    Update flash.lua

--- Windows/nvim-lazyvim/lua/plugins/flash.lua
+++ Windows/nvim-lazyvim/lua/plugins/flash.lua
@@ -1,9 +1,12 @@
-return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    keys = {
+-- ~/.config/nvim/lua/plugins/flash.lua
+-- C:/Users/user_name/AppData/Local/nvim/lua/plugins/flash.lua
+-- Flash Setup
+local M = {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
@@ -11,3 +14,4 @@ return {
         { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
 }
+return M
