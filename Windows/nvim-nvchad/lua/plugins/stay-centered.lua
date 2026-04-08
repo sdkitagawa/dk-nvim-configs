98b11b9cf1e6b15fe11ba0839a9870bfa129f4b7
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-04-09 00:29:42 +0900

    Update stay-centered.lua

--- Windows/nvim-nvchad/lua/plugins/stay-centered.lua
+++ Windows/nvim-nvchad/lua/plugins/stay-centered.lua
@@ -1,3 +1,6 @@
+-- ~/.config/nvim/lua/plugins/stay-centered
+-- C:/Users/user_name/AppData/Local/nvim/lua/plugins/stay-centered.lua
+-- Stay Centered Setup
 return {
     "arnamak/stay-centered.nvim",
     config = function()
