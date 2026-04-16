925a4bd74c9c0e35c4f357fee03b04a03e1094bb
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-04-17 02:43:24 +0900

    feat(keymaps): add horizontal and vertical toggleterm terminal mappings for ventura

--- macOS/Ventura/nvim-lazyvim/lua/config/keymaps.lua
+++ macOS/Ventura/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Terminal = require("toggleterm.terminal").Terminal
+local vterm = Terminal:new({ direction = "vertical" })
+
+vim.keymap.set("n", "<leader>h", function()
+    require("toggleterm.terminal").Terminal:new({ direction = "horizontal" }):toggle()
+end, { desc = "Open Horizontal Terminal" })
+
+vim.keymap.set("n", "<leader>v", function()
+    vterm:toggle()
+end, { desc = "Vertical Terminal", remap = false })
